f138eec7a26951cbb494f70a600fe926ca9131fe
hahy23 <gkswntjr2329@gmail.com>
2024-06-15 13:37:07 +0900

    객체지향프로그래밍 복습

--- ProjectOOP/src/Others/MyOOP.java
+++ ProjectOOP/src/Others/MyOOP.java
@@ -1,10 +1,35 @@
 package Others;
 
 public class MyOOP {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		delimiter = "----";
+		
+		printA();
+		printA();
+		printB();
+		printB();
+		
+		delimiter = "****";
+		printA();
+		printA();
+		printB();
+		printB();
+		
 	}
+	public static String delimiter = "";
+	public static void printA() {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	}
+	
+	public static void printB() {
+		System.out.println(delimiter);
+		System.out.println("B");
+		System.out.println("B");
+		
+	}
+	//..
 
 }
