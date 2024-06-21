e3c75b7fe7058665f71e0967df8915c4edf09882
hahy23 <gkswntjr2329@gmail.com>
2024-06-21 16:49:31 +0900

    static의 특징

---
+++ ProjectOOP/src/Others/StaticApp.java
@@ -0,0 +1,18 @@
+package Others;
+
+class Foo{
+	
+	public static String classVar = "I class var";
+	public String instanceVar = "I instance var";
+	
+}
+
+
+public class StaticApp {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
